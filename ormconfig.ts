4aedd655e0e18cfee5b7d615260c2f6286f78bf4
Samy951 <samy.hamlat01@gmail.com>
2024-11-20 15:13:10 +0100

    project setup

---
+++ ormconfig.ts
@@ -0,0 +1,13 @@
+import * as dotenv from 'dotenv';
+import { DataSource } from 'typeorm';
+
+dotenv.config();
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  url: process.env.DATABASE_URL,
+  entities: ['dist/**/*.entity{.ts,.js}'],
+  migrations: ['dist/migrations/*{.ts,.js}'],
+  synchronize: false,
+  logging: process.env.NODE_ENV === 'development',
+});
